4b511d419713a161187f8b5a100c90226718dd4a
dileshwar-01 <rajputdileshwar16@gmail.com>
2025-07-17 16:45:15 +0530

    added schema models for user and product data

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 
 //APP CONFIG
 const app = express();
 const port = process.env.PORT || 4000
 connectDB()
-
+connectCloudinary()
 //MIDDLEWARES
 app.use(express.json())
 app.use(cors())
