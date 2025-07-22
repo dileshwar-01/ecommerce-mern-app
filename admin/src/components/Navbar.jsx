17a8d4dfee7bc9cc8f835ec9f2d547b43e75bcdd
dileshwar-01 <rajputdileshwar16@gmail.com>
2025-07-22 22:51:44 +0530

    added admin login/logout using backend API

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
-const Navbar = () => {
+import {assets} from '../assets/assets.js'
+const Navbar = ({setToken}) => {
   return (
-    <div>
-      
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+      <img className='w-[max(6%,70px)]' src={assets.logo} alt="" />
+      <button onClick={()=>setToken('')} className= 'bg-gray-600 cursor-pointer text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
