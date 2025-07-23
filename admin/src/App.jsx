48f66cb8fd7a59506e7880607a7eb310e7fb4922
dileshwar-01 <rajputdileshwar16@gmail.com>
2025-07-23 17:52:59 +0530

    displayed products list and added remove product feature in admin panel using backend API's

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,6 +9,7 @@ import Login from './components/Login'
 import { ToastContainer, toast } from 'react-toastify';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = ""
 
 
 const App = () => {
@@ -31,8 +32,8 @@ const App = () => {
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base ">
               <Routes>
                 <Route path="/add" element={<Add token={token}/>} />
-                <Route path="/list" element={<List />} />
-                <Route path="/orders" element={<Orders />} />
+                <Route path="/list" element={<List token={token}/>} />
+                <Route path="/orders" element={<Orders token={token}/>} />
               </Routes>
             </div>
           </div>
