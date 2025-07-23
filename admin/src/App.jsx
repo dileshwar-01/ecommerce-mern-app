8de29665bd46492c1ed6a13a5cf2a4dc403caa7a
dileshwar-01 <rajputdileshwar16@gmail.com>
2025-07-23 10:10:35 +0530

    added products to database from admin panel using backend API

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
             <Sidebar />
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base ">
               <Routes>
-                <Route path="/add" element={<Add />} />
+                <Route path="/add" element={<Add token={token}/>} />
                 <Route path="/list" element={<List />} />
                 <Route path="/orders" element={<Orders />} />
               </Routes>
