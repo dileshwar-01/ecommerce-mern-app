1886eedc98a18f7471121ac4810ab55731c99c79
dileshwar-01 <rajputdileshwar16@gmail.com>
2025-07-28 09:33:44 +0530

    added -change order status from admin panel

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -26,6 +26,19 @@ const Orders = ({token}) => {
   useEffect(()=>{
     fetchAllOrders();
   },[token])
+
+  const statusHandler = async(e, orderId) => {
+    try {
+      const response = await axios.post(backendUrl+'/api/order/status', {orderId,status:e.target.value} ,{headers:{token}});
+      if(response.data.success){
+        await fetchAllOrders();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(response.data.message);
+    }
+  }
+
   return (
     <div>
     <h3>Order Page</h3>
@@ -59,7 +72,7 @@ const Orders = ({token}) => {
             <p>Date : {new Date(order.date).toLocaleDateString()}</p>
           </div>
           <p className='text-sm sm:text-[15px]'>{currency}{order.amount}</p>
-          <select className='p-2 font-semibold'>
+          <select onChange={(e)=>statusHandler(e,order._id)} value={order.status} className='p-2 font-semibold'>
             <option value="Order Placed">Order Placed</option>
             <option value="Packing">Packing</option>
             <option value="Shipped">Shipped</option>
